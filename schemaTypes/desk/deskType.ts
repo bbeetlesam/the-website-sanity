a1c8aaaeeb897588b72183d03e611dde9e4eac52
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-09-08 02:07:55 +0700

    feat: added Desk type

---
+++ schemaTypes/desk/deskType.ts
@@ -0,0 +1,51 @@
+import { defineField, defineType } from 'sanity';
+
+const deskType = defineType({
+  name: 'desk',
+  title: 'Desk',
+  type: 'document',
+
+  fields: [
+    // Identifier for the Desk
+    defineField({
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+
+    // Size of the Desk
+    defineField({
+      name: 'size',
+      title: 'Size',
+      type: 'object',
+      validation: (rule) => rule.required(),
+      fields: [
+        defineField({
+          name: 'width',
+          title: 'Width',
+          type: 'number',
+          validation: (rule) => rule.required(),
+        }),
+
+        defineField({
+          name: 'height',
+          title: 'Height',
+          type: 'number',
+          validation: (rule) => rule.required(),
+        }),
+      ],
+    }),
+
+    // List of Desk Item(s) placed in Desk
+    defineField({
+      name: 'deskItems',
+      title: 'Desk Items',
+      type: 'array',
+      of: [{ type: 'desk-item' }],
+      validation: (rule) => rule.required(),
+    }),
+  ],
+});
+
+export default deskType;
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,12 @@ import authorType from './blog/authorType';
 import blogType from './blog/blogType';
 import navItemType from './navigation/navItemType';
 import deskItemType from './desk/deskItemType';
+import deskType from './desk/deskType';
 
-export const schemaTypes = [blogType, authorType, navItemType, deskItemType];
+export const schemaTypes = [
+  blogType,
+  authorType,
+  navItemType,
+  deskItemType,
+  deskType,
+];
