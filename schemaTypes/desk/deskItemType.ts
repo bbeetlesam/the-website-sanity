11c300eb90baa87051adba31cc41d73395effc0d
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-09-08 01:40:34 +0700

    feat: added deskItem type

---
+++ schemaTypes/desk/deskItemType.ts
@@ -0,0 +1,109 @@
+import { defineField, defineType } from 'sanity';
+
+const deskItemType = defineType({
+  name: 'desk-item',
+  title: 'Desk Item',
+  type: 'object',
+
+  fields: [
+    // A unique identifier for each Desk item
+    defineField({
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+
+    // Image icon used for the Desk item
+    defineField({
+      name: 'icon',
+      title: 'Image Icon',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+
+    // Image alt name for the Desk item's image icon
+    defineField({
+      name: 'imageAlt',
+      title: 'Image Alt',
+      type: 'string',
+    }),
+
+    // Position (X and Y) of the Desk item
+    defineField({
+      name: 'position',
+      title: 'Position',
+      type: 'object',
+      validation: (rule) => rule.required(),
+      fields: [
+        defineField({
+          name: 'x',
+          title: 'X',
+          type: 'number',
+        }),
+
+        defineField({
+          name: 'y',
+          title: 'Y',
+          type: 'number',
+        }),
+      ],
+    }),
+
+    // The size in pixels of the Desk item
+    defineField({
+      name: 'size',
+      title: 'Size',
+      type: 'number',
+    }),
+
+    // The rotation angle in degree of the Desk item
+    defineField({
+      name: 'rotation',
+      title: 'Rotation',
+      type: 'number',
+    }),
+
+    // Focus Frame configuration for the Desk item
+    defineField({
+      name: 'focusFrame',
+      title: 'Focus Frame',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'size',
+          title: 'Size',
+          type: 'number',
+        }),
+
+        defineField({
+          name: 'cornerSize',
+          title: 'Corner Size',
+          type: 'number',
+        }),
+
+        defineField({
+          name: 'cornerThickness',
+          title: 'Corner Thickness',
+          type: 'number',
+        }),
+
+        defineField({
+          name: 'edgeOffset',
+          title: 'Edge Offset',
+          type: 'number',
+        }),
+      ],
+    }),
+
+    // Navigation configuration for the Desk item
+    defineField({
+      name: 'navigation',
+      title: 'Navigation',
+      type: 'reference',
+      to: [{ type: 'nav-item' }],
+    }),
+  ],
+});
+
+export default deskItemType;
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import authorType from './blog/authorType';
 import blogType from './blog/blogType';
 import navItemType from './navigation/navItemType';
+import deskItemType from './desk/deskItemType';
 
-export const schemaTypes = [blogType, authorType, navItemType];
+export const schemaTypes = [blogType, authorType, navItemType, deskItemType];
